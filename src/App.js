39d2f7ad594b92e396452a208b5e1078aa33c41c
DHughes666 <graziemolto6@gmail.com>
2023-09-21 20:51:28 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Score from "./objectUpdate_exercise_component";
+import { Canvas } from "./objectUpdate_exercise_component";
 
 const App = () => {
   return (
     <>
       <Score />
+      <hr />
+      <hr />
+      <Canvas />
     </>
 
   )
--- src/objectUpdate_exercise_component.jsx
+++ src/objectUpdate_exercise_component.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Background from "./background_component";
+import Box from "./box_component";
 
 /**
  * This form has a few bugs. 
@@ -64,4 +66,73 @@ const Score = () => {
     )
 }
 
+
+/**
+ * There is a draggable box on a static background. 
+ * You can change the box’s color using the select input.
+
+    But there is a bug. If you move the box first, 
+    and then change its color, the background 
+    (which isn’t supposed to move!) will “jump” to the box position. 
+    But this should not happen: the Background’s position prop is set 
+    to initialPosition, which is { x: 0, y: 0 }. 
+    Why is the background moving after the color change?
+
+    Find the bug and fix it.
+ */
+
+const initialPosition = {
+    x: 0,
+    y: 0
+};
+
+const Canvas = () => {
+    const [shape, setShape] = useState({
+        color: 'orange',
+        position: initialPosition
+    });
+
+    const handleMove = (dx, dy) => {
+        setShape({
+            ...shape,
+            position: {
+                x: shape.position.x + dx,
+                y: shape.position.y + dy,
+            }
+        })
+    }
+
+    const handleColorChange = (e) => {
+        setShape({
+            ...shape,
+            color: e.target.value
+        });
+    }
+
+    return (
+        <>
+            <select
+                value={shape.color}
+                onChange={handleColorChange}
+            >
+            <option value="orange">orange</option>
+            <option value='lightpink'>lightpink</option>
+            <option value="aliceblue">aliceblue</option>
+            </select>
+            <Background 
+                position={initialPosition}
+            />
+
+            <Box
+                color={shape.color}
+                position={shape.position}
+                onMove={handleMove}
+            >
+                Drag me!
+            </Box>
+        </>
+    )
+}
+
 export default Score;
+export {Canvas};
