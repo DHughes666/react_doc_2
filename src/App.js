97c85d6e1edb3998436f7c1e423bd29a684c3247
DHughes666 <graziemolto6@gmail.com>
2023-09-30 21:08:26 +0100

    Escape-Hatches => Refs-Dom-Manipulation: ...In progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import Play from "./interaction/effects/effects-component";
 import AppComp from "./interaction/context-components/context-components-exercises/app-component-exercise";
-import Counter from "./interaction/escape-hatches/refs/refs-component";
-import { Stopwatch1 } from "./interaction/escape-hatches/refs/timer-component";
-import Chat from "./interaction/escape-hatches/refs/refs-exercises/refExercise1-component";
-import Toggle from "./interaction/escape-hatches/refs/refs-exercises/refExercise2-component";
-import Dashboard from "./interaction/escape-hatches/refs/refs-exercises/refExercise3-component";
-import Chatty from "./interaction/escape-hatches/refs/refs-exercises/refExercise4-component";
-
+import { Form, CatFriends } from "./interaction/escape-hatches/refs-dom-manipulation/refs-dom-component";
 const App = () => {
   return (
     <>
@@ -18,27 +12,11 @@ const App = () => {
       <br />
       <br />
       <br />
-      <Counter />
+      <Form />
       <br />
       <br />
       <br />
-      <Stopwatch1 />
-      <br />
-      <br />
-      <br />
-      <Chat />
-      <br />
-      <br />
-      <br />
-      <Toggle />
-      <br />
-      <br />
-      <br />
-      <Dashboard />
-      <br />
-      <br />
-      <br />
-      <Chatty />
+      <CatFriends />
       
     </>
   )
