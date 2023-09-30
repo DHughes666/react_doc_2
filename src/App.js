1808628dbcd2a6d5acdcc06860fe5e6b0879246c
DHughes666 <graziemolto6@gmail.com>
2023-09-30 14:30:35 +0100

    Context-Reducers ...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Play from "./interaction/effects/effects-component";
 import Page from "./interaction/context-components/context-component1";
 import AppComp from "./interaction/context-components/context-components-exercises/app-component-exercise";
+import TaskApp from "./interaction/context-Reducer/appReduce-component.jsx";
 
 
 const App = () => {
   return (
     <>
       <Play />
+      <br />
+      <br />
+      <br />
+      <br />
       <Page />
+      <br />
+      <br />
+      <br />
+      <br />
       <AppComp />
+      <br />
+      <br />
+      <br />
+      <br />
+      <TaskApp />
+      
     </>
   )
 }
