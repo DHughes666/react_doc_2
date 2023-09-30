9706e4e708c2de078d47691939ca6703a9867a92
DHughes666 <graziemolto6@gmail.com>
2023-09-30 11:33:31 +0100

    Context... lesson learned

--- src/interaction/context-components/context-component1.jsx
+++ src/interaction/context-components/context-component1.jsx
@@ -4,22 +4,46 @@ import Section from "./section-component";
 const Page = () => {
     return (
         <Section>
-            <Heading>Title</Heading>
-            <Section>
-                <Heading>Heading</Heading>
-                <Heading>Heading</Heading>
-                <Heading>Heading</Heading>
-                <Section>
-                    <Heading>Heading</Heading>
-                    <Heading>Heading</Heading>
-                    <Heading>Heading</Heading>
-                    <Section>
-                        <Heading>Heading</Heading>
-                        <Heading>Heading</Heading>
-                        <Heading>Heading</Heading>
-                    </Section>
-                </Section>
-            </Section>
+            <Heading>My Profile</Heading>
+            <Post 
+                title="Hello traveller!"
+                body="Read about my adventures."
+            />
+            <AllPosts />
+        </Section>
+    )
+}
+
+const AllPosts = () => {
+    return (
+        <Section>
+            <Heading>Posts</Heading>
+            <RecentPosts />
+        </Section>
+    )
+}
+
+const RecentPosts = () => {
+    return (
+        <Section>
+            <Heading>Recent Posts</Heading>
+            <Post 
+                title="Flavours of Lisbon"
+                body="...those pasteis de nata!"
+            />
+            <Post 
+                title="Buenos Aires in the rhythm of tango"
+                body="I loved it!"
+            />
+        </Section>
+    )
+}
+
+const Post = ({ title, body }) => {
+    return (
+        <Section isFancy={true}>
+            <Heading>{title}</Heading>
+            <p><i>{body}</i></p>
         </Section>
     )
 }
